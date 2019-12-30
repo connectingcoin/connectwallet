68a9ad923273932af16065787ca79006481284e1
Chatter Tech <reneekrom28@gmail.com>
2019-12-30 11:07:15 +0300

    updates

---
+++ src/api/coins/XCON.js
@@ -0,0 +1,13 @@
+const { createERC20 } = require('./ERC20')
+const { MAINNET } = require('../../const/')
+
+module.exports = createERC20({
+    symbol: 'XCON',
+    name: 'Connect coin',
+    color: '#000000',
+    contract_address: '0x015df42d36Bc851c7F15f80bd1D4e8dBF02aed0c',
+    labels: 'xcon ethereum token erc20 ecr20',
+    coin_decimals: 18,
+    price_decimals: 2,
+    networks_availables: [MAINNET]
+})
